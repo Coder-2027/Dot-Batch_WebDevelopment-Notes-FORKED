9de293a2dc40e8081f67700a0e7cc424caa2fcda
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-09 21:07:15 +0530

    Update index.js

--- React/01 - React - Basic/top-course-starter/src/index.js
+++ React/01 - React - Basic/top-course-starter/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";                //is added to add toast (side mein appear hone waali pop ups) search on google
+import "react-toastify/dist/ReactToastify.css";                  //Google
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <App />
-    <ToastContainer />
+    <ToastContainer />              //can be added anywhere either in the parent or the child where it is to be used
   </>
 );
