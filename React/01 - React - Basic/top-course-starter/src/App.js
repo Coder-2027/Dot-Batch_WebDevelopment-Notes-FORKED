d159574990272eccd3d27c97bed7eeb2d8b697b0
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-09 21:23:35 +0530

    Update App.js

--- React/01 - React - Basic/top-course-starter/src/App.js
+++ React/01 - React - Basic/top-course-starter/src/App.js
@@ -4,12 +4,12 @@ import Filter from "./Components/Filter";
 import Cards from "./Components/Cards";
 import Spinner from "./Components/Spinner";
 import { apiUrl, filterData } from "./data.js";
-import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
+import { toast } from "react-toastify";                        //******
+import { useEffect, useState } from "react";                //******
 
 const App = () => {
-  // const [courses, setCourses] = useState(null);
-  const [courses, setCourses] = useState([]);
+  // const [courses, setCourses] = useState(null);            due to this when courses was being passed in line 52 then it was being passed as null since data processing took time
+  const [courses, setCourses] = useState([]);                        //******
   const [laoding, setLoading] = useState(true);
   const [category, setCategory] = useState([]);
 
@@ -23,14 +23,14 @@ const App = () => {
       setCourses(output.data);
       // setCourses(output);
     } catch (err) {
-      toast.error("Something Went Wrong");
+      toast.error("Something Went Wrong");                //******
     }
     setLoading(false);
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData();                                  //fetching of data is generally done in useEffect hook
+  }, []);                                          //runs only on first rendering
 
   return (
     <div className="min-h-screen flex-col flex bg-bgDark2">
@@ -49,11 +49,7 @@ const App = () => {
 
         <div className="w-11/12 max-w-[1200px] min-h-[50vh] mx-auto flex flex-wrap justify-center items-center">
           {
-            laoding ? (
-              <Spinner />
-            ) : (
-              <Cards courses={courses} category={category} />
-            )
+            laoding ? (<Spinner />) : (<Cards courses={courses} category={category} />)
           }
         </div>
       </div>
