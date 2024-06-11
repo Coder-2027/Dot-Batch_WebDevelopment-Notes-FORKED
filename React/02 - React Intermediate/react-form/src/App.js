8823bdd9adfc562d78b116766367957b7c757dcd
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-11 15:32:15 +0530

    Update App.js

--- React/02 - React Intermediate/react-form/src/App.js
+++ React/02 - React Intermediate/react-form/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  // const [firstName, setFirstName] = useState("");
+  // const [firstName, setFirstName] = useState("");                    //handling single state
   // const [lastName, setLastName] = useState("");
 
   // function changeFirstNameHandler(event) {
@@ -17,7 +17,7 @@ function App() {
   //   setLastName(event.target.value);
   // }
 
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState(            {              //using curly braces we are storing an object inside formData [Handling multiple states here]
     firstName: "",
     lastName: "",
     email: "",
@@ -34,8 +34,11 @@ function App() {
     setFormData((prevFormData) => {
       // console.log(prevFormData);
       return {
-        ...prevFormData,
-        [name]: type === "checkbox" ? checked : value,
+        ...prevFormData,                                                        //*****
+        [name]: type === "checkbox" ? checked : value,                        //we have used name here to identify call is made from which input
+        //[event.target.name] : event.target.value                      //using : because we are storing it as key value pairs inside object
+        //to access an attribute we use [] brackets like [name] -> SYNTAX
+        // here we have used checked logic to access value of checkbox
       };
     });
   }
@@ -49,14 +52,14 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={submitHandler}>
+      <form onSubmit={submitHandler}>            //onSubmit initiated by pressing submit button in form
           <input
             type="text"
             placeholder="Enter First name"
             // onChange={changeFirstNameHandler}
             onChange={changeHandler}
-            name="firstName"
-            value={formData.firstName}
+            name="firstName"                      //good practice is to use same name as written inside useState beacuse it is laso being used in line 38
+            value={formData.firstName}                    //Controlled components here input is also maintaining its state [during rerendering it executes and this rerendeing happens during execution of useState]                        will be used further in projects
           />
 
           <br />
@@ -99,23 +102,23 @@ function App() {
             name="isAgree"
             onChange={changeHandler}
             id="isAgree"
-            checked={formData.isAgree}
+            checked={formData.isAgree}                //incase of checkbox we dont have value but we have checked state which is to be updated
           />
 
-          <label htmlFor="isAgree">Agree Term and Condition</label>
+          <label htmlFor="isAgree">Agree Term and Condition</label>            //// in jsx we use htmlFor rather than for attribute
 
           <br />
           <br />
 
-          <fieldset> 
-
+          <fieldset>         //to group elements in a box we use field set tag
+          <legend> Mode:</legend>            ////// //to add caption to the field
           <input
             type="radio"
             name="mode"
             id="Dark-mode"
-            value="Dark-mode"
+            value="Dark-mode"                            //*****
             onChange={changeHandler}
-            checked={formData.mode === "Dark-mode"}
+            checked={formData.mode === "Dark-mode"}              //***** (putting a boolean value in checkbox)
           />
 
           <label htmlFor="Dark-mode">Dark Mode</label>
@@ -126,7 +129,7 @@ function App() {
             id="Light-mode"
             value="Light-mode"
             onChange={changeHandler}
-            checked={formData.mode === "Light-mode"}
+            checked={formData.mode === "Light-mode"}              //*****
           />
           <label htmlFor="Light-mode">Light Mode</label>
         </fieldset>
@@ -148,7 +151,8 @@ function App() {
 
         <br /><br />
 
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" />            //or we can make it with the help of button tag
+            //point to remember is that whenever inside form submit button is pressed then in form onSubmit event is initiated 
       </form>
     </div>
   );
