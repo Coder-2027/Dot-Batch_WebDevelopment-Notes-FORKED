0da290ca54a7b8e9ac3ac68e7a806fccac50050d
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-13 16:43:32 +0530

    Update useGif.js

--- React/03 - React Advanced/random-meme/src/hooks/useGif.js
+++ React/03 - React Advanced/random-meme/src/hooks/useGif.js
@@ -6,8 +6,9 @@ import { useState } from 'react';
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+//it would be better if we first write our code as usual and then convert it into custom hook
 
-const useGif = (tag) => {
+const useGif = (tag) => {                          //on basis of tag we will decide from where call is madeeither from random file or tag file
   const [gif, setGif] = useState('');
   const [loading, setLoading] = useState(false);
 
