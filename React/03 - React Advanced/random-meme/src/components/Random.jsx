280393fbec3b3bb1a0d4199959548e52a2dbedc6
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-13 16:39:17 +0530

    Update Random.jsx

--- React/03 - React Advanced/random-meme/src/components/Random.jsx
+++ React/03 - React Advanced/random-meme/src/components/Random.jsx
@@ -1,7 +1,7 @@
 import useGif from "../hooks/useGif";
 import Spinner from "./Spinner";
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;                      //another way to get environment variable - process.env.{variable_name}
 
 const Random = () => {
   // const [gif, setGif] = useState("");
@@ -11,7 +11,7 @@ const Random = () => {
   //   setLoading(true)
   //   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
-  //   const { data } = await axios.get(url);
+  //   const { data } = await axios.get(url);                                //another way to fetch data from url but first we need to install it it also returns a promise here it automatically returns in json format we need not to convert it (use of fetch and axios depends on use case)
   //   const imageSource = data.data.images.downsized_large.url;
   //   setGif(imageSource);
   //   console.log(imageSource);
@@ -19,10 +19,10 @@ const Random = () => {
   // }
 
   // useEffect(() => {
-  //   fetchData();
+  //   fetchData();                                        //all api call are made under useEffect
   // }, []);
 
-  const { gif, loading, fetchData } = useGif();
+  const { gif, loading, fetchData } = useGif();                    //we use custom hook becuase in json file it is optimal to keep the code above return as small as possible but inside return we ca n keep any number of lines
 
   return (
     <div className="w-1/2 h-[450px] bg-green-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
