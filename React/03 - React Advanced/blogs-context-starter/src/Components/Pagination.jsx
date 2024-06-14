0179dee035addb2265024b8c03f51b5b96d7a0f6
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-14 12:10:55 +0530

    Update Pagination.jsx

--- React/03 - React Advanced/blogs-context-starter/src/Components/Pagination.jsx
+++ React/03 - React Advanced/blogs-context-starter/src/Components/Pagination.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { AppContext } from '../Context/AppContext'
+import { AppContext } from '../Context/AppContext'                        //******
 
 const Pagination = () => {
 
@@ -8,7 +8,7 @@ const Pagination = () => {
   return (
     <div className='class="fixed bottom-0 inset-x-0 bg-white py-2 border-t-2 border-t-gray-300 w-full'>
       <div className='flex items-center gap-x-3 w-11/12 max-w-2xl mx-auto'>
-        {
+        {                                        //using this curly braces whenever we are required to apply some condition
           page > 1 &&
           <button className='border-2 border-gray-300 py-1 px-4 rounded-md' onClick={() => handlerPageChange(page - 1)}>Previous</button>
         }
