a29c6b6850107e6dacee641ecc0b72a04bbaf26f
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-15 10:07:44 +0530

    Update store.js

--- React/03 - React Advanced/redux-counter/src/redux/store.js
+++ React/03 - React Advanced/redux-counter/src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
 import CounterSlice from './slices/CounterSlice'
 
-export const store = configureStore({
+export const store = configureStore({          //it creates a global store containing all slices
   reducer: {
-    counter: CounterSlice
+    counter: CounterSlice                  //here counter is the name of slice and CounterSlice is the name of reducer which we have exported at the end of file
   },
 })
