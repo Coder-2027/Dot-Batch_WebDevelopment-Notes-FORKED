ea8295b4d6b6aa11066fe151bb0d300690989e49
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-15 14:58:20 +0530

    Update CounterSlice.jsx

--- React/03 - React Advanced/redux-counter/src/redux/slices/CounterSlice.jsx
+++ React/03 - React Advanced/redux-counter/src/redux/slices/CounterSlice.jsx
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {                                
+const initialState = {                                //it is an object may be because we can have more than one variable inside our initial state
     value:0,
 }
 
@@ -8,7 +8,7 @@ export const CounterSlice = createSlice({                            //inside th
     name:"counter",
     initialState,
     reducers: {                                                //inside this we define functionalities of our application
-        increment : (state) => {                                             //here state is our current state
+        increment : (state) => {                                             //here state is our current state -> or the object of initialState
             state.value += 1;
         },
         decrement: (state) => {
