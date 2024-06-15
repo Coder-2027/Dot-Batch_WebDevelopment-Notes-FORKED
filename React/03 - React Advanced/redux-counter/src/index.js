d2c5e29633392f4af60d93ce52148fa72e66a5ec
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-15 10:15:14 +0530

    Update index.js

--- React/03 - React Advanced/redux-counter/src/index.js
+++ React/03 - React Advanced/redux-counter/src/index.js
@@ -8,7 +8,9 @@ import { store } from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Provider  store={store} >                  //to link our redux store with react 
+  <Provider  store={store} >                  //to link our redux store with react ,just same as we used to link our contextAPI with our react we used to write <AppContextProvider>
+  //Now app and all its components can now access redux functionalities
+  //where store is the global store which we exported
       <App />
   </Provider>
 );
