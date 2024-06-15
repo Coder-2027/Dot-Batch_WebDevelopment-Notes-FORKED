238f6e1dbe61d3bc50d298657159540b4419df2c
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-15 14:52:44 +0530

    Update Counter.jsx

--- React/03 - React Advanced/redux-counter/src/components/Counter.jsx
+++ React/03 - React Advanced/redux-counter/src/components/Counter.jsx
@@ -4,13 +4,22 @@ import { increment, decrement } from '../redux/slices/CounterSlice';
 
 const Counter = () => {
 
-    const count = useSelector((state) => state.counter.value );
-    const dispatch = useDispatch();
+    const count = useSelector((state) => state.counter.value );                    //to fetch data from slice we use this hook
+    // useSelector Hook: This is a hook provided by react-redux that allows you to extract data from the Redux store state. It takes a selector function as its argument.
+// Selector Function: (state) => state.counter.value is the selector function. It receives the entire store state as its argument and returns a part of it. In this case, it’s returning state.counter.value, which would be the current value of your counter.
+// state.counter.value: This is the part of the state that the selector function is accessing. It assumes that your store’s state has a counter object with a value property in it.
+
+    //here in state.counter -> counter is the name of slice from where value is to be accessed
+    //here first the call goes to store then it goes to the particular slice from where data is to be taken -------> flow of data
+    
+    const dispatch = useDispatch();                //inorder to call the functions which we have fetched from action creater we need to use useDispatch hook
+
+                                //if we want to use states then we useSelector hook and if we want to use functions then we use useDispatch hook
 
   return (
     <div>
       <button
-      onClick={() => dispatch(increment())}
+      onClick={() => dispatch(increment())}                        //first we need to import increment and decrement
       >
         Increment
       </button>
