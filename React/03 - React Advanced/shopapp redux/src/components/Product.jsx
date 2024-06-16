a6a721d73bdebc7e794549cb7a7911370bd67d37
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-16 12:05:23 +0530

    Update Product.jsx

--- React/03 - React Advanced/shopapp redux/src/components/Product.jsx
+++ React/03 - React Advanced/shopapp redux/src/components/Product.jsx
@@ -36,6 +36,19 @@ const Product = ({post}) => {
         </div>
         
         {
+{/*       The some() method checks if any array elements pass a test (provided as a callback function).
+
+          The some() method executes the callback function once for each array element.
+          
+          The some() method returns true (and stops) if the function returns true for one of the array elements.
+          
+          The some() method returns false if the function returns false for all of the array elements.
+          
+          The some() method does not execute the function for empty array elements.
+          
+          The some() method does not change the original array. */}
+
+
           cart.some((p) => p.id == post.id) ?
           (<button
           className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
