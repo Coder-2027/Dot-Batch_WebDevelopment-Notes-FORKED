ebce5eac53feb4db2abd55bb7abb82d86b0a3d92
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-20 23:05:44 +0530

    Update createTodo.js

--- Backend/02 - TodoApp/controllers/createTodo.js
+++ Backend/02 - TodoApp/controllers/createTodo.js
@@ -3,12 +3,12 @@ const Todo = require("../models/Todo");
 
 //define route handler
 
-exports.createTodo = async(req,res) => {
+exports.createTodo = async(req,res) => {                        //making a network call is always done inside async function , here we are making a call to database in ln 11
     try {
             //extract title and desxcription from reauest body
             const {title,description} = req.body;
             //create a new Todo Obj and insert in DB
-            const response = await Todo.create({title,description});
+            const response = await Todo.create({title,description});                    //here create function is used for creating and inserting data in our database
             //send a json response with a success flag
             res.status(200).json(
                 {
