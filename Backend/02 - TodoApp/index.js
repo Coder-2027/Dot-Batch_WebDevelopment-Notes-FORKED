500d3e0f7583e358b226cfd76666f777803b5986
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-20 23:24:01 +0530

    Update index.js

--- Backend/02 - TodoApp/index.js
+++ Backend/02 - TodoApp/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express");                //good practice is that all importing statements should be written in starting
 const app = express();
 
 //load config from env file
@@ -12,7 +12,9 @@ app.use(express.json());                          //another syntax for body pars
 const todoRoutes = require("./routes/todos");
 
 //mount the todo API routes
-app.use("/api/v1", todoRoutes);
+app.use("/api/v1", todoRoutes);              //now due to this in url we will have first localhost, then api/v1 and then we would have route contained in todoRoutes
+//here v1 is used to define version like if we roll out another version v2 and want our previous client to smoothly use older version we use v1 and for newer rolled out version we use version v2
+//would be applied to all the route defined in todoRoutes
 
 //start server
 app.listen(PORT, () => {
