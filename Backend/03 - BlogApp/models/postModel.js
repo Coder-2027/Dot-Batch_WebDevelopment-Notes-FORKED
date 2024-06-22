70c452223b5c0add952b5c19dee5227d75055a05
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-22 09:29:55 +0530

    Update postModel.js

--- Backend/03 - BlogApp/models/postModel.js
+++ Backend/03 - BlogApp/models/postModel.js
@@ -12,9 +12,11 @@ const postSchema = new mongoose.Schema({
         type : String,
         required : true
     } ,
-    likes : [{
+    likes : [{                                                    //an array of likes containing data of users those who have liked the post
+                                                            //good practice to not to put actual data but put reference
+        //here we will just send id of object who is liking our post
         type : mongoose.Schema.Types.ObjectId,
-        ref : "Like",
+        ref : "Like",                            //reference to like model which will contain data of user who has liked our post
     }],
     comments : [{
         type : mongoose.Schema.Types.ObjectId,
