da6983da1caf7e6be80b6d545d2e641781aafe70
Coder-2027 <165177615+Coder-2027@users.noreply.github.com>
2024-06-26 23:12:01 +0530

    Update File.js

--- Backend/07 - FileApp/models/File.js
+++ Backend/07 - FileApp/models/File.js
@@ -19,23 +19,27 @@ const fileSchema = new mongoose.Schema({
 
 require("dotenv").config();
 
-fileSchema.post("save", async function (doc) {
+//post method is applied on schema, it is always essential to add this post method before making of model
+// post middleware is used when we want to perform an action just after creating an entry in database similiarly pre middleware is used just before creating entry in
+// database
+
+fileSchema.post("save", async function (doc) {                        //here doc refers to the document saved on database
     try {
         console.log("DOC : ", doc)
 
         // transporter
-        const transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
+        const transporter = nodemailer.createTransport({                        //nodemailer is used for sending mails
+            host: process.env.MAIL_HOST,                        //here as a host we will be using server of google mail
             auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS
+                user: process.env.MAIL_USER,                    //username and password via which we would be sending mails
+                pass: process.env.MAIL_PASS                        //generate this passkey from manage googlle account > search > app password > generate it from there
             },
         })
 
         // send mail 
-        const info = await transporter.sendMail({
+        const info = await transporter.sendMail({                        //used for sending mails
             from: 'From Priyansh',
-            to: doc.email,
+            to: doc.email,                            //to the email id supplied in doc
             subject: "New File Uploaded to Cloudinary",
             html: `<h2>File Uploaded</h2> <br> view now - <a href="${doc.fileUrl}">CLick Here</a>`
         })
